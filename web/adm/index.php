a87bc2857cc6eb18d22745af55dce33cab88a640
Guinso <guinso@gmail.com>
2015-01-03 18:25:14 +0800

    apply web page post loading process

--- web/adm/index.php
+++ web/adm/index.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <html lang="en" ng-app="MyApp">
 	 <head>
-	 	<meta charset='utf-8' name="Top Brilliant" content="Top Brilliant" />
+	 	<meta charset='utf-8' name="System Admin" content="System Admin" />
 	 	<meta name="viewport" content="width=device-width, initial-scale=1">	
-		<title>Administrator Page</title>
+		<title>System Admin</title>
 	 	
 		<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />
-	
-		<link href="../lib/bootstrap/css/bootstrap-3.1.1.css" rel="stylesheet">
-		<link href="../lib/bootstrap/css/bootstrap-theme-3.1.1.css" rel="stylesheet" -->
-		<!-- link href="css/bootstrap.min.css" rel="stylesheet" -->
+
+		<link href="../lib/bootstrap/css/bootstrap-3.1.1.min.css" rel="stylesheet">
+		<link href="../lib/bootstrap/css/bootstrap-theme-3.1.1.css" rel="stylesheet">
 		<link href="../lib/bootstrap-datepicker/css/datepicker3.css" rel="stylesheet">
 		<link href="../lib/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
-
 		<link href="css/override.css" rel="stylesheet">
+	
+		
+		<!-- link href="css/bootstrap.min.css" rel="stylesheet" -->
 		
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -22,87 +23,30 @@
 		     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 		   <![endif]-->
 	 </head>
-  	<body ng-clock ng-controller='AppCtrl'>
-  		<nav class='navbar navbar-inverse' role='navigation' ng-show='!navigation.plainPage'>
-  			<!-- Brand and toggle get grouped for better mobile display -->
-  			<div class='container-fluid'>
-  				
-  				<div class='navbar-header'>
-  					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-			        	<span class="sr-only">Toggle navigation</span>
-			        	<span class="icon-bar"></span>
-			        	<span class="icon-bar"></span>
-			        	<span class="icon-bar"></span>
-			      	</button>
-			      	<a class="navbar-brand" href="#">Administrator</a>
-  				</div>
-  				
-  				<!-- Collect the nav links, forms, and other content for toggling -->
-	  			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	  			
-	  				<!-- Menu Item -->
-	  				<ul class='nav navbar-nav'>
-						<li class='dropdown' ng-class="{active: navigation.paths[1]=='config'}">
-						  	<a class='dropdown-toggle hover-cursor' data-toggle='dropdown' >
-						  	Setting<span class='caret'></span></a>
-						  	<ul class='dropdown-menu'>
-						  		<li><a href='#/config/account-main'>User</a></li>
-						  		<li><a href='#/config/schedule'>Schedule</a></li>
-						  		<li><a href='#/config/update'>Update</a></li>
-						  		<li><a href='#/config/misc'>Misc</a></li>
-					  		</ul>
-						</li>	
-	  				</ul>
-
-	  				<!-- Login Menu -->
-	  				<ul class='nav navbar-nav navbar-right'>
-	  					<li ng-show='user.login'><a href=''>{{'Welcome, ' + user.username}}</a></li>
-	  					<li ng-show='user.login'><a href='' ng-click='logout()'>Logout</a></li>
-			  		</ul>
-	  			</div>
-  			</div>
-  		</nav>
-
-  		<div class='container-fluid' id='mainMsg' ng-controller='MsgCtrl' ng-class="{'avoid-clicks': !msg.show}">
-		  	<!-- Message Container -->
-		  	<div class='row'>
-		  		<div class='alert alert-danger my-show-hide drop-shadow'
-		  			ng-show='msg.show'
-		  			ng-class="{	'alert-danger': msg.status=='error', 
-		  						'alert-warning': msg.status=='warning',
-		  						'alert-success': msg.status=='ok'}">
-		  			<button type="button" class="close" 
-		  					ng-click='msg.hideMsg()' aria-hidden="true">
-		  				&times;
-		  			</button>
-		  			
-		  			<span ng-show='!msg.lock' class='glyphicon glyphicon-pushpin' ng-click='msg.cancelHide()'></span>
-		  			<span ng-show='msg.lock' class='glyphicon glyphicon-lock' ng-click='msg.hideMsg()'></span>
-		  			<span ng-bind='msg.message'></span>
-		  		</div>
-		  	</div>
-	  	</div>
-	  	
-	  	<div class='container-fluid'  ng-controller='LoaderCtrl' >
-		  	<!-- Loader Container -->
-		  	<div class='row' id="loader" ng-if="loader.show">
-		  		<div class="loader-big">
-		  			<img src="img/loader.gif">
-		  		</div>
-		  	</div>
-	  	</div>
-	  	
-	  	<!-- Main Container -->
-  		<div ng-view></div>
+  	<body>
+  		<div id='loading-msg'>
+  			Please wait while loading
+  		</div>
+  		<div id='bodyy' ng-controller='AppCtrl'>
+  		</div>
   	</body>
   	
   	<script src="../lib/jquery/jquery-2.1.1.min.js"></script>
+	<script type="text/javascript">
+		$('#bodyy').hide();
+		$.ajaxSetup({async:false});
+		$('#bodyy').load('page/mainFrame.html');
+		
+		//$.getScript("../lib/moment/moment-2.6.0.min.js");
+		$.ajaxSetup({async:true});
+	</script>
+	
   	<script src="../lib/moment/moment-2.6.0.min.js"></script>
   	
 	<script src="../lib/bootstrap/js/bootstrap-3.1.1.js"></script>
 	<script src="../lib/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
 	<script src="../lib/bootstrap-datetimepicker/js/bootstrap-datetimepicker-3.0.0-min.js"></script>
-	
+
 	<script src="../lib/angular/angular-1.2.13.js"></script>
 	<script src="../lib/angular/angular-resource-1.2.13.js"></script>
 	<script src="../lib/angular/angular-route-1.2.13.js"></script>
@@ -114,8 +58,6 @@
 	
 	<script src='js/app.js'></script>
 	<script src='js/directive.js'></script>
-	<script src='js/filter.js'></script>
-	<script src='js/service.js'></script>
 	<script src='js/controller.js'></script>
 	<script src="js/router.js"></script>
 	
@@ -149,4 +91,9 @@ rerJS(WEB_DIR . '/partials');
 rerJS(WEB_DIR . '/modules');
 ?>
 
+<script type="text/javascript">
+	$('#loading-msg').hide();
+	$('#bodyy').show();
+</script>
+		
 </html>
