1da3228c7b569ab740844885175b29130c75b218
Guinso <guinso@gmail.com>
2015-01-06 11:38:57 +0800

    add missing constants

--- web/maintenance.php
+++ web/maintenance.php
@@ -1,6 +1,12 @@
 <?php 
 //load minimum setting file
 define('ROOT_DIR', dirname(__FILE__));
+define('DS' , DIRECTORY_SEPARATOR);
+
+define('API_DIR', dirname(__FILE__));
+define('ADM_DIR', ROOT_DIR . DIRECTORY_SEPARATOR . 'adm');
+define('ADM_API_DIR', ADM_DIR . DIRECTORY_SEPARATOR . 'api');
+
 require_once ROOT_DIR . DIRECTORY_SEPARATOR . 'api' . DIRECTORY_SEPARATOR . 
 			'libs' . DIRECTORY_SEPARATOR . 'ig' . DIRECTORY_SEPARATOR . 'igConfig.php';
 
