d641e6c71ef6bc027daee65dc880e6250009fb58
Guinso <guinso@gmail.com>
2015-02-04 02:09:10 +0800

    refine loader

--- web/adm/partials/com-ctrl.js
+++ web/adm/partials/com-ctrl.js
@@ -1,5 +1,41 @@
 angular.module('MyApp').
 
+//circle arc
+directive('circleArc', function() {
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs) {
+        	var canvas = $(element)[0];
+	      	var context = canvas.getContext('2d');
+	      	var x = canvas.width / 2;
+	      	var y = canvas.height / 2;
+	      	var radius = x * 0.8;
+	      	var startAngle = 1.2 * Math.PI;
+	      	var endAngle = 1.0 * Math.PI;
+	      	var counterClockwise = false;
+
+	      	context.beginPath();
+	      	context.arc(x, y, radius, startAngle, endAngle, counterClockwise);
+	      	context.lineWidth = 5;
+
+	      	// line color
+	     	context.strokeStyle = 'black';
+	      	context.stroke();
+        }
+	}
+}).
+
+//Loader Directive
+directive('mdlLoader', function() {
+    return {
+        restrict: 'A',
+        scope: {
+        	show: '=',
+        },
+        templateUrl: 'partials/modal-loader.html'
+    }
+}).
+
 //GUI generator Search Directive
 directive('accountSearch', function() {
     return {
