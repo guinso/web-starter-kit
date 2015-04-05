9b22befd13b781f2c9c92cd72ab8d850c9b08ae3
Guinso <guinso@gmail.com>
2015-04-06 01:55:44 +0800

    fix Ig autoloader

--- web/api/libs/Ig/autoloader.php
+++ web/api/libs/Ig/autoloader.php
@@ -15,4 +15,19 @@ require_once $igDir . DIRECTORY_SEPARATOR . 'fileUtil.php';
 
 require_once $igDir . DIRECTORY_SEPARATOR . 'igConfig.php';
 require_once $igDir . DIRECTORY_SEPARATOR . 'igConfigLoader.php';
+
+
+//implement PSR-4 autoloading
+function IgAutoload($className)
+{
+	$baseDir = dirname(dirname(__FILE__));
+
+	$filePath = $baseDir . DIRECTORY_SEPARATOR .
+	str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
+
+	if(is_readable($filePath)) {
+		require_once $filePath;
+	}
+}
+spl_autoload_register('IgAutoload', true, true);
 ?>
