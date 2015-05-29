c1664c166c00b5631f5f162f556efd20f0dbc271
Guinso <guinso@gmail.com>
2015-05-29 18:04:29 +0800

    remove obsoleted files

--- web/adm/api/index.php
+++ web/adm/api/index.php
@@ -1,32 +1,5 @@
 <?php 
-//uncomment this to observe error message
-//x error_reporting(E_ALL);
-//x ini_set( 'display_errors','1');
-
-/********************** library and settings **********/
-$dir = dirname(dirname(dirname(dirname(__FILE__))));
-include $dir . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'bootstrap.php';
-
-/********************** Modules ***********************/
-
-//automatic load all module files
-\Ig\Util::recursiveDir(
-	dirname(__FILE__) . DIRECTORY_SEPARATOR . 'modules',
-	function($filePath){
-		include_once $filePath;
-	}, 
-	'/^.+\.php$/');
-
-//place API definition here
-require_once dirname(__FILE__) . DIRECTORY_SEPARATOR . 'api.php';
-
-function home() {
-	echo 'Web Admin.<br/>';
-	echo  \Ig\Date::getDate() . "<br/>";
-}
-
-/********************* Execute ***********************/
-
-getRoute()->get('/', 'home');
-getRoute()->run();
+include_once dirname(dirname(dirname(dirname(__FILE__)))) .
+	DIRECTORY_SEPARATOR . 'nextApp' .
+	DIRECTORY_SEPARATOR . 'mainAdmin.php';
 ?>
